93dcacb10acb03b65d4774d08a7f8e86eeca0eec
Kevin Jilissen <info@kevinjilissen.nl>
2020-03-09 13:42:19 +0100

    Add proper configuration for test case coverage and linting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,19 @@ module.exports = {
     node: true,
     mocha: true
   },
+  plugins: [
+    "chai-friendly"
+  ],
   extends: [
     "airbnb-typescript/base",
+    "plugin:chai-expect/recommended",
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'linebreak-style': process.env.NODE_ENV === 'production' ? ['error', 'windows'] : ['off', 'windows'],
+    "@typescript-eslint/no-unused-expressions": 0,
+    "chai-friendly/no-unused-expressions": 2
   },
   parserOptions: {
     project: './tsconfig.json',
