abd09573d97119ffe8a1b9e2c84fe5972f5aacc8
Kevin Jilissen <info@kevinjilissen.nl>
2020-04-28 18:00:54 +0200

    Add middleware for the policy definition.

---
+++ src/middleware/policy-middleware.ts
@@ -0,0 +1,44 @@
+import { RequestHandler, Response } from 'express';
+import { PolicyImplementation } from '../controller/policy';
+import { RequestWithToken } from './token-middleware';
+
+/**
+ * This class is responsible for:
+ * - enforcing a given policy implementation as middleware.
+ */
+export default class PolicyMiddleware {
+  /**
+   * A reference to the policy to be used by this middleware instance.
+   */
+  private readonly policy: PolicyImplementation;
+
+  /**
+   * Creates a new policy middleware instance.
+   * @param policy - the policy to be used by this middleware.
+   */
+  public constructor(policy: PolicyImplementation) {
+    this.policy = policy;
+  }
+
+  /**
+   * Middleware handler for enforcing the policy.
+   * @param req - the express request to handle.
+   * @param res - the express response object.
+   * @param next - the express next function to continue processing of the request.
+   */
+  public async handle(req: RequestWithToken, res: Response, next: Function): Promise<void> {
+    if (await this.policy(req)) {
+      next();
+      return;
+    }
+
+    res.status(401).end('You have insufficient permissions for the requested action.');
+  }
+
+  /**
+   * @returns a middleware handler to be used by express.
+   */
+  public getMiddleware(): RequestHandler {
+    return this.handle.bind(this);
+  }
+}
---
+++ test/unit/middleware/policy-middleware.ts
@@ -0,0 +1,45 @@
+import express, { Application, Response } from 'express';
+import { expect, request } from 'chai';
+import PolicyMiddleware from '../../../src/middleware/policy-middleware';
+
+describe('PolicyMiddleware', (): void => {
+  let ctx: {
+    app: Application,
+    middleware: PolicyMiddleware,
+    value: boolean,
+  };
+
+  before(async () => {
+    // Initialize context
+    ctx = {
+      app: express(),
+      middleware: new PolicyMiddleware(async () => ctx.value),
+      value: false,
+    };
+
+    ctx.app.use(ctx.middleware.getMiddleware());
+    ctx.app.use((req: any, res: Response) => {
+      res.end('Success');
+    });
+  });
+
+  afterEach(() => {
+    ctx.value = false;
+  });
+
+  describe('#handle', () => {
+    it('should give an HTTP 401 when policy implementation returns false', async () => {
+      const res = await request(ctx.app)
+        .get('/');
+
+      expect(res.status).to.equal(401);
+    });
+    it('should give an HTTP 200 when policy implementation returns true', async () => {
+      ctx.value = true;
+      const res = await request(ctx.app)
+        .get('/');
+
+      expect(res.status).to.equal(200);
+    });
+  });
+});
