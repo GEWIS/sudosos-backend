ba2c2702ce5dfaba2bafdd591b649eb16dc87e84
Bram Wieringa <b.wieringa31@gmail.com>
2021-01-17 16:25:47 +0100

    Added Product Helper Functions

---
+++ src/controller/response/product-response.ts
@@ -0,0 +1,43 @@
+/**
+ *  SudoSOS back-end API service.
+ *  Copyright (C) 2020  Study association GEWIS
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import { DineroObject } from 'dinero.js';
+import User from '../../entity/user/user';
+import ProductCategory from '../../entity/product/product-category';
+
+/**
+ * @typedef {BaseEntity} ProductResponse
+ * @property {number} id.required - The unique id of the product.
+ * @property {string} name.required - The name of the product.
+ * @property {Dinero.model} price.required - The price of the product.
+ * @property {User.model} owner.required - The owner of the product.
+ * @property {ProductCategory.model} category.required - The category the product belongs to.
+ * @property {string} picture.required - The URL to the picture representing this product.
+ * @property {number} alcoholPercentage - The percentage of alcohol in this product.
+ */
+export default interface ProductResponse {
+  id: number,
+  createdAt: Date,
+  updatedAt: Date,
+  revision: number,
+  name: string,
+  price: DineroObject,
+  owner: User,
+  category: ProductCategory,
+  picture: String,
+  alcoholPercentage: number,
+}
---
+++ src/helpers/mappers.ts
@@ -0,0 +1,70 @@
+/**
+ *  SudoSOS back-end API service.
+ *  Copyright (C) 2020  Study association GEWIS
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import {createQueryBuilder} from 'typeorm';
+import Product from "../entity/product/product";
+import ProductRevision from "../entity/product/product-revision";
+import User from "../entity/user/user";
+import UpdatedProduct from "../entity/product/updated-product";
+import ProductResponse from "../controller/response/product-response";
+
+export default class Mappers {
+    public async getProducts(owner: User = null, returnUpdated: boolean = true): Promise<ProductResponse[]> {
+        const builder = createQueryBuilder(Product, "product")
+        .innerJoinAndSelect(ProductRevision, "productrevision", "product.id = productrevision.product " +
+            "AND product.currentRevision = productrevision.revision")
+        .select([
+            'product.id', 'product.createdAt', 'productrevision.updatedAt', 'productrevision.revision',
+            'productrevision.name', 'productrevision.price', 'product.owner', 'productrevision.category',
+            'productrevision.picture', 'productrevision.alcoholpercentage'
+        ]);
+        if (owner !== null) {
+            builder.where("product.owner = :owner", {owner: owner.id});
+        }
+        if (!returnUpdated) {
+            builder.where(builder => {
+                const subQuery = builder.subQuery()
+                    .select("updatedproduct.product")
+                    .from(UpdatedProduct, "updatedproduct")
+                    .getQuery();
+                return `product.id NOT IN (:subQuery)`, {subQuery: subQuery};
+            });
+        }
+        return await builder.getMany() as ProductResponse[];
+    }
+
+    public async getUpdatedProducts(owner: User = null): Promise<ProductResponse[]> {
+        const builder= createQueryBuilder(Product)
+            .innerJoin(UpdatedProduct, "updatedproduct", "product.id = updatedproduct.product")
+            .select([
+                'product.id', 'product.createdAt', 'updatedproduct.updatedAt', 'updatedproduct.name',
+                'updatedproduct.price', 'product.owner', 'updatedproduct.category', 'updatedproduct.picture',
+                'updatedproduct.alcoholpercentage'
+            ]);
+        if (owner !== null) {
+            builder.where("product.owner = :owner", {owner: owner.id});
+        }
+        return await builder.getMany() as ProductResponse[];
+    }
+
+    public async getProductsWithUpdates(owner: User = null): Promise<ProductResponse[]> {
+        const products = await this.getProducts(owner);
+        const updatedProducts = await this.getUpdatedProducts(owner);
+
+        return products.concat(updatedProducts) as ProductResponse[];
+    }
+}
