be511c3383f5121d155f4fa4d0d00e5f444b59de
Samuel <samuelincognito@gmail.com>
2022-04-16 22:48:30 +0200

    For some reason we get a entity manager overlap?

--- src/index.ts
+++ src/index.ts
@@ -222,13 +222,13 @@ export default async function createApp(): Promise<Application> {
   application.tasks = [syncBalances];
 
   if (process.env.ENABLE_LDAP === 'true') {
-    // await ADService.syncSharedAccounts().then(() => Gewis.syncUserRoles(application.roleManager));
-    const syncADGroups = cron.schedule('*/10 * * * *', async () => {
-      logger.debug('Syncing AD.');
-      await ADService.syncSharedAccounts().then(() => Gewis.syncUserRoles(application.roleManager));
-      logger.debug('Synced AD');
-    });
-    application.tasks.push(syncADGroups);
+    await ADService.syncSharedAccounts().then(() => Gewis.syncUserRoles(application.roleManager));
+    // const syncADGroups = cron.schedule('*/10 * * * *', async () => {
+    //   logger.debug('Syncing AD.');
+    //   await ADService.syncSharedAccounts().then(() => Gewis.syncUserRoles(application.roleManager));
+    //   logger.debug('Synced AD');
+    // });
+    // application.tasks.push(syncADGroups);
   }
 
   // REMOVE LATER
