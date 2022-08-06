7d82c8ba5ab0b9e30de0ba0cc026c80a12ab24e9
justsamuel <samuel.oosterholt20001004@gmail.com>
2022-08-06 17:27:40 +0200

    Create and use Reset Tokens

---
+++ src/entity/authenticator/reset-token.ts
@@ -0,0 +1,31 @@
+/**
+ *  SudoSOS back-end API service.
+ *  Copyright (C) 2020  Study association GEWIS
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import { Column } from 'typeorm';
+import HashBasedAuthenticationMethod from './hash-based-authentication-method';
+
+/**
+ * @typedef {HashBasedAuthenticationMethod} ResetToken
+ * @property {string} expires.required - The end date from which the token is expired
+ */
+export default class ResetToken extends HashBasedAuthenticationMethod {
+  @Column({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  public expires: Date;
+}
