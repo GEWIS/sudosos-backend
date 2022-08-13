3990a3a8963cfe38eb6876fbc4715b101bb1a591
justsamuel <samuel.oosterholt20001004@gmail.com>
2022-08-13 20:04:32 +0200

    Requesting a reset link now also sends an email
    Signed-off-by: justsamuel <samuel.oosterholt20001004@gmail.com>

---
+++ src/mailer/templates/password-reset.ts
@@ -0,0 +1,103 @@
+/**
+ *  SudoSOS back-end API service.
+ *  Copyright (C) 2020  Study association GEWIS
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import MailTemplate, { Language, MailLanguageMap } from './mail-template';
+import { signatureDutch, signatureEnglish } from './signature';
+import MailContent from './mail-content';
+import { ResetTokenInfo } from '../../service/authentication-service';
+
+interface WelcomeWithResetOptions {
+  name: string;
+  email: string,
+  resetTokenInfo: ResetTokenInfo,
+  url?: string;
+}
+
+const passwordResetDutch = new MailContent<WelcomeWithResetOptions>({
+  getHTML: (context) => `
+<p>Beste ${context.name},</p>
+
+<p>Een wachtwoord reset voor dit email adres is aangevraagd. Om het proces te voltooien, gebruik de volgende link elk moment binnen de komende 60 minuten: </p>
+
+<p>${context.url + '/passwordreset?token=' + context.resetTokenInfo.password + '&email=' + context.email}</p>
+
+<p>Als u geen wachtwoord reset heeft aangevraagd, kunt u deze e-mail veilig negeren en uw huidige inloggegevens gebruiken.</p>
+
+<p>Tot op de borrel!</p>
+
+${signatureDutch}`,
+  getSubject: () => 'Welkom bij SudoSOS!',
+  getText: (context) => `
+Beste ${context.name},
+
+Een wachtwoord reset voor dit email adres is aangevraagd. Om het proces te voltooien, gebruik de volgende link elk moment binnen de komende 60 minuten: 
+
+${context.url + '/passwordreset?token=' + context.resetTokenInfo.password + '&email=' + context.email}
+
+Als u geen wachtwoord reset heeft aangevraagd, kunt u deze e-mail veilig negeren en uw huidige inloggegevens gebruiken.
+
+Tot op de borrel!
+
+Met vriendelijke groet,
+SudoSOS`,
+});
+
+const passwordResetEnglish = new MailContent<WelcomeWithResetOptions>({
+  getHTML: (context) => `
+<p>Dear ${context.name},</p>
+
+<p>A password reset for this email address has been requested. To complete the process, use the following link any time within the next 60 minutes: </p>
+
+<p>${context.url + '/passwordreset?token=' + context.resetTokenInfo.password + '&email=' + context.email}</p>
+
+<p>If you have not requested a password reset, you can safely ignore this email and use your current login information.</p>
+
+<p>See you on the borrel!</p>
+
+${signatureEnglish}`,
+  getSubject: () => 'Welcome to SudoSOS!',
+  getText: (context) => `
+Dear ${context.name},
+
+A password reset for this email address has been requested. To complete the process, use the following link any time within the next 60 minutes: 
+
+${context.url + '/passwordreset?token=' + context.resetTokenInfo.password + '&email=' + context.email}
+
+If you have not requested a password reset, you can safely ignore this email and use your current login information.
+
+See you on the borrel!
+
+Kind regards,
+SudoSOS`,
+});
+
+const mailContents: MailLanguageMap<WelcomeWithResetOptions> = {
+  [Language.DUTCH]: passwordResetDutch,
+  [Language.ENGLISH]: passwordResetEnglish,
+};
+
+export default class PasswordReset extends MailTemplate<WelcomeWithResetOptions> {
+  public constructor(options: WelcomeWithResetOptions) {
+    const opt: WelcomeWithResetOptions = {
+      ...options,
+    };
+    if (!options.url) {
+      opt.url = process.env.url;
+    }
+    super(opt, mailContents);
+  }
+}
