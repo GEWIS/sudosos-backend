873ad09ad36cc2d0037f0a3da278d82ee4fadceb
justsamuel <samuel.oosterholt20001004@gmail.com>
2022-08-16 14:05:42 +0200

    small bugfix
    Signed-off-by: justsamuel <samuel.oosterholt20001004@gmail.com>

--- src/entity/authenticator/ldap-authenticator.ts
+++ src/entity/authenticator/ldap-authenticator.ts
@@ -29,7 +29,7 @@ import AuthenticationMethod from './authentication-method';
 export default class LDAPAuthenticator extends AuthenticationMethod {
   @Column({
     length: 32,
-    type: process.env.TYPEORM_CONNECTION === 'sqlite' ? 'character' : 'char',
+    type: 'bytes',
   })
   public UUID: string;
 }
