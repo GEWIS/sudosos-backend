cacee108a3a65eebb07ea05c13b41522e35a9b64
Samuel <samuelincognito@gmail.com>
2023-07-12 22:14:42 +0200

    Typo

--- src/controller/request/invoice-request.ts
+++ src/controller/request/invoice-request.ts
@@ -62,7 +62,7 @@ export interface CreateInvoiceParams extends BaseInvoice {
  * @property {integer} forId.required - The recipient of the Invoice.
  * @property {integer} byId - The creator of the Invoice, defaults to the ID of the requester.
  * @property {string} addressee.required - Name of the addressed.
- * @property {string} description.required - The description of the invoice is the .
+ * @property {string} description.required - The description of the invoice.
  * @property {Array.<InvoiceEntryRequest>} customEntries - Custom entries to be added to the invoice
  * @property {Array.<integer>} transactionIDs - IDs of the transactions to add to the Invoice.
  * @property {string} fromDate - For creating an Invoice for all transactions from a specific date.
