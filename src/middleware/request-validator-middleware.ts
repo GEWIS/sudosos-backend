60b2874af3bb608313ff7a6f7cf4a5ee68ee7acf
Samuel <samuelincognito@gmail.com>
2023-12-26 13:51:50 +0100

    Continued CI/CD testsuite bug fixing.

--- src/middleware/request-validator-middleware.ts
+++ src/middleware/request-validator-middleware.ts
@@ -42,7 +42,7 @@ export default class RequestValidatorMiddleware {
   public constructor(specification: SwaggerSpecification, validator: BodyValidator) {
     this.specification = specification;
     this.validator = validator;
-
+    console.error(!specification.components?.schemas);
     if (!specification.components?.schemas[validator.modelName] && !specification.definitions[validator.modelName]) {
       throw new Error(`Model '${validator.modelName}' not defined.`);
     }
