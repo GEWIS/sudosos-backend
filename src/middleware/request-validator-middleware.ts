48963ae750c7932f5870626168af5bafee9605e2
Samuel <samuelincognito@gmail.com>
2023-12-26 19:06:02 +0100

    Fixed the stupid mistake

--- src/middleware/request-validator-middleware.ts
+++ src/middleware/request-validator-middleware.ts
@@ -42,7 +42,6 @@ export default class RequestValidatorMiddleware {
   public constructor(specification: SwaggerSpecification, validator: BodyValidator) {
     this.specification = specification;
     this.validator = validator;
-    console.error(!!specification.components?.schemas, validator.modelName, specification.components?.schemas[validator.modelName]);
     if (!specification.components?.schemas[validator.modelName] && !specification.definitions[validator.modelName]) {
       throw new Error(`Model '${validator.modelName}' not defined.`);
     }
