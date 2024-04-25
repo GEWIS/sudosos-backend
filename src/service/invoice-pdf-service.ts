9004c886e5294034701b6014d948ca04b2357c6e
Samuel <samuelincognito@gmail.com>
2024-04-25 20:50:34 +0200

    hotfix: fixed test case

--- src/service/invoice-pdf-service.ts
+++ src/service/invoice-pdf-service.ts
@@ -235,8 +235,7 @@ export default class InvoicePdfService {
       const buffer = Buffer.from(await blob.arrayBuffer());
       return this.pdfGenerator.fileService.uploadInvoicePdf(invoice, buffer, invoice.to, hashJSON(this.getInvoiceParameters(invoice)));
     }).catch((res: any) => {
-      console.error('Invoice failed with ', res);
-      throw new Error('Invoice generation failed');
+      throw new Error(`Invoice generation failed for ${JSON.stringify(res, null, 2)}`);
     });
   }
 }
