7f245394ee29088558977ce30c074d5298a49eeb
Samuel <samuelincognito@gmail.com>
2024-05-18 11:51:52 +0200

    chore: update NOTICE year

--- src/declaration/express-swagger-generator/index.d.ts
+++ src/declaration/express-swagger-generator/index.d.ts
@@ -1,6 +1,6 @@
 /**
  *  SudoSOS back-end API service.
- *  Copyright (C) 2020  Study association GEWIS
+ *  Copyright (C) 2024  Study association GEWIS
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
@@ -15,6 +15,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 declare module 'express-swagger-generator' {
   import express from 'express';
 
