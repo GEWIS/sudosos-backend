32395780c13cd5c84ad559a09e5f90782ca1d1d0
Samuel <samuelincognito@gmail.com>
2024-05-18 21:11:31 +0200

    feat: added eslint-plugin-github-commit-hash to force that github deps have a commit hash

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,31 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    mocha: true
+    mocha: true,
   },
   plugins: [
     'chai-friendly',
     'header',
     'import',
+    'github-commit-hash',
   ],
   extends: [
     'airbnb-typescript/base',
     'plugin:chai-expect/recommended',
   ],
+  overrides: [
+    {
+      files: ['package.json'],
+      extends: ['./.eslintrc.package.json.js'],
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -21,11 +35,8 @@ module.exports = {
     '@typescript-eslint/no-unused-expressions': 'off',
     'chai-friendly/no-unused-expressions': 'error',
     '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/await-thenable' : 'warn',
-    "header/header": [2, 'NOTICE'],
+    '@typescript-eslint/await-thenable': 'warn',
+    'header/header': [2, 'NOTICE'],
     'class-methods-use-this': 'off',
-  },
-  parserOptions: {
-    project: './tsconfig.json',
   },
 };
---
+++ .eslintrc.package.json.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: 'jsonc-eslint-parser',
+  rules: {
+    'github-commit-hash/check-git-commit-hash': 'error',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-expressions': 'off',
+    '@typescript-eslint/await-thenable': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/dot-notation': 'off',
+    '@typescript-eslint/no-implied-eval': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
+    '@typescript-eslint/return-await': 'off',
+    'header/header': 'off',
+  },
+};
