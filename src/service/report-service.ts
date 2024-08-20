0c2c9cbad98c1f8370ce33af3a40f35db04b755b
Samuel <samuelincognito@gmail.com>
2024-08-20 12:18:35 +0200

    test: fixed test cases and fixed linting

--- src/service/report-service.ts
+++ src/service/report-service.ts
@@ -21,7 +21,7 @@ import ProductRevision from '../entity/product/product-revision';
 import Dinero from 'dinero.js';
 import VatGroup from '../entity/vat-group';
 import ProductCategory from '../entity/product/product-category';
-import {toLocalMySQLString, toMySQLString} from '../helpers/timestamps';
+import { toLocalMySQLString, toMySQLString } from '../helpers/timestamps';
 import SubTransactionRow from '../entity/transactions/sub-transaction-row';
 import SubTransaction from '../entity/transactions/sub-transaction';
 import { asDinero, asNumber } from '../helpers/validators';
@@ -76,7 +76,7 @@ export interface SalesReport extends Report {}
 
 export interface BuyerReport extends Report {}
 
-interface ReportParameters {
+export interface ReportParameters {
   fromDate: Date,
   tillDate: Date,
   forId: number,
