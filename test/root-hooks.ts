f938a97d2f212898b46c3859592803cbf0f13a65
Samuel <samuelincognito@gmail.com>
2024-09-16 11:56:57 +0200

    chore: update NOTICE

--- test/root-hooks.ts
+++ test/root-hooks.ts
@@ -14,7 +14,10 @@
  *
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  @license
  */
+
 import * as mocha from 'mocha';
 import sinon from 'sinon';
 import nodemailer, { Transporter } from 'nodemailer';
