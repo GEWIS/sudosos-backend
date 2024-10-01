3da4df25c77b613299c3eefbd153ab591a1ed148
Sven Mokveld <SvenMokveldje@gmail.com>
2024-10-01 16:08:40 +0200

    feat: check eligible users
    check which users are eligible for notificition or fine

--- src/entity/user/user.ts
+++ src/entity/user/user.ts
@@ -70,6 +70,13 @@ export const NotifyDebtUserTypes: UserType[] = [
 ];
 
 /**
+ * All users that could make inactive administrative costs.
+ */
+export const EligibleInactiveUsers: UserType[] = [
+  UserType.LOCAL_ADMIN, UserType.LOCAL_USER, UserType.MEMBER,
+];
+
+/**
  * @typedef {BaseEntity} User
  * @property {string} firstName.required - First name of the user.
  * @property {string} lastName - Last name of the user.
