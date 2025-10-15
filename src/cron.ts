c74790150a8511cd0e32df10f667fb6b3044ce54
Samuel <samuelincognito@gmail.com>
2025-10-15 22:57:43 +0200

    refactor: move sync service initialization to a factory class

--- src/cron.ts
+++ src/cron.ts
@@ -34,10 +34,8 @@ import RoleManager from './rbac/role-manager';
 import Gewis from './gewis/gewis';
 import EventService from './service/event-service';
 import DefaultRoles from './rbac/default-roles';
-import LdapSyncService from './service/sync/user/ldap-sync-service';
-import { UserSyncService } from './service/sync/user/user-sync-service';
+import UserSyncServiceFactory from './service/sync/user/user-sync-service-factory';
 import UserSyncManager from './service/sync/user/user-sync-manager';
-import GewisDBSyncService from './gewis/service/gewisdb-sync-service';
 import getAppLogger from './helpers/logging';
 import ServerSettingsStore from './server-settings/server-settings-store';
 
@@ -109,17 +107,12 @@ async function createCronTasks(): Promise<void> {
   // INJECT GEWIS BINDINGS
   Gewis.overwriteBindings();
 
-  const syncServices: UserSyncService[] = [];
-
-  if (process.env.ENABLE_LDAP === 'true') {
-    const ldapSyncService = new LdapSyncService(application.roleManager);
-    syncServices.push(ldapSyncService);
-  }
-
-  if (process.env.GEWISDB_API_KEY && process.env.GEWISDB_API_URL) {
-    const gewisDBSyncService = new GewisDBSyncService();
-    syncServices.push(gewisDBSyncService);
-  }
+  // Create sync services using the factory
+  const syncServiceFactory = new UserSyncServiceFactory();
+  const syncServices = syncServiceFactory.createSyncServices({
+    roleManager: application.roleManager,
+    manager: application.connection.manager,
+  });
 
   if (syncServices.length !== 0) {
     const syncManager = new UserSyncManager(syncServices);
