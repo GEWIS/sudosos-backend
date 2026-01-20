938cd15aa0a849211514f031854657b83fc1481c
Samuel <samuel@gewis.nl>
2026-01-20 18:43:04 +0100

    fix: pos cashier roles being emptied on update (#707)

--- src/service/point-of-sale-service.ts
+++ src/service/point-of-sale-service.ts
@@ -183,8 +183,9 @@ export default class PointOfSaleService {
     await PointOfSaleRevision.save(pointOfSaleRevision);
 
     // Set roles
-    base.cashierRoles = await Role.find({ where: { id: In(update.cashierRoleIds ?? []) } });
-
+    if (Array.isArray(update.cashierRoleIds)) {
+      base.cashierRoles = await Role.find({ where: { id: In(update.cashierRoleIds) } });
+    }
     // Increment current revision.
     // eslint-disable-next-line no-param-reassign
     base.currentRevision = base.currentRevision ? base.currentRevision + 1 : 1;
