a67cc5de889af87b216cf86bc8bf25d5ed8fc8c3
Robin van Dijke <robinvandijke2002@gmail.com>
2026-05-22 13:39:04 +0200

    fix: init script (#934)

--- init_scripts/00_make_sudosos_data_dirs.sh
+++ init_scripts/00_make_sudosos_data_dirs.sh
@@ -4,20 +4,20 @@ set -e
 mkdir -p /app/config
 chown node /app/config
 
-mkdir -p /app/out/data
-chown node /app/out/data
+mkdir -p /app/data
+chown node /app/data
 
-mkdir -p /app/out/data/banners
-chown node /app/out/data/banners
+mkdir -p /app/data/banners
+chown node /app/data/banners
 
-mkdir -p /app/out/data/products
-chown node /app/out/data/products
+mkdir -p /app/data/products
+chown node /app/data/products
 
-mkdir -p /app/out/data/invoices
-chown node /app/out/data/invoices
+mkdir -p /app/data/invoices
+chown node /app/data/invoices
 
-mkdir -p /app/out/data/payout_requests
-chown node /app/out/data/payout_requests
+mkdir -p /app/data/payout_requests
+chown node /app/data/payout_requests
 
-mkdir -p /app/out/data/seller_payouts
-chown node /app/out/data/seller_payouts
+mkdir -p /app/data/seller_payouts
+chown node /app/data/seller_payouts
